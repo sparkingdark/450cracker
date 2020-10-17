c1241db27e271664e2f56699491145f735305e8f
sparkingdark <debomastet335@gmail.com>
2020-10-17 14:45:19 +0530

    done with recursive

--- max min element in array/recur_max_min.py
+++ max min element in array/recur_max_min.py
@@ -1,23 +1,34 @@
-def max_duo(a, b):
-    if a > b:
-        return a
-    return b
-
-
-def min_duo(a, b):
-    if a < b:
-        return a
-    return b
-
-
-def recur_max_min(arr, size):
-    if size == 1:
-        return (arr[0], arr[0])
-    elif size == 2:
-        return (max_duo(arr[0], arr[1]), min_duo(arr[0], arr[1]))
-    else:
-        pass
+def getMinMax(low, high, arr): 
+    arr_max = arr[low] 
+    arr_min = arr[low] 
+      
+    # If there is only one element  
+    if low == high: 
+        arr_max = arr[low] 
+        arr_min = arr[low] 
+        return (arr_max, arr_min) 
+          
+    # If there is only two element 
+    elif high == low + 1: 
+        if arr[low] > arr[high]: 
+            arr_max = arr[low] 
+            arr_min = arr[high] 
+        else: 
+            arr_max = arr[high] 
+            arr_min = arr[low] 
+        return (arr_max, arr_min) 
+    else: 
+          
+        # If there are more than 2 elements 
+        mid = int((low + high) / 2) 
+        arr_max1, arr_min1 = getMinMax(low, mid, arr) 
+        arr_max2, arr_min2 = getMinMax(mid + 1, high, arr) 
+  
+    return (max(arr_max1, arr_max2), min(arr_min1, arr_min2)) 
+  
+        
 
 
 if __name__ == "__main__":
-    pass
+    print(getMinMax(0,4,[1,2,3,45,5]))
+
